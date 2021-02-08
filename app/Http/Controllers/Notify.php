5132a45c21ce45cd92007d72b92e8eb58406ee1d
puzzle <happypuzzle@126.com>
2021-02-08 22:56:56 +0800

    up wecaht notify

--- app/Http/Controllers/Notify.php
+++ app/Http/Controllers/Notify.php
@@ -21,7 +21,8 @@ class Notify extends Controller
             abort(404);
         }
 
-        // Order::paySuccess($result['out_trade_no'], $result['']);
+        // {"appid":"wx2ed04ec3264a1b24","bank_type":"CMB_CREDIT","cash_fee":"1","fee_type":"CNY","is_subscribe":"Y","mch_id":"1604952925","nonce_str":"MG2W1W3OPPW1berk","openid":"o9tFBwKdeBs55IcQHyTrnE1Ue-cw","out_trade_no":"wechat20210208220205bIZsN","result_code":"SUCCESS","return_code":"SUCCESS","sign":"6114FEC635C559FA533ABC16B0A031E6","time_end":"20210208222309","total_fee":"1","trade_type":"JSAPI","transaction_id":"4200000780202102081584484302"}
+        Order::paySuccess($result['out_trade_no'], $result['transaction_id']);
 
         return $wechat->success();
     }
